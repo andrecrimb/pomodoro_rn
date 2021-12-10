23dd26d8ad210da62b7657b9cd61b8b52a428fd1
Andre Rosa <ar@gsmk.de>
2021-12-10 14:23:39 +0100

    Include disable feature to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,12 +8,13 @@ type Props = {
   color?: ButtonColor
   onPress: () => void
   title: string
+  disabled?: boolean
 }
 //#endregion
 
-export default ({ color = 'default', onPress, title }: Props) => {
+export default ({ color = 'default', onPress, title, disabled = false }: Props) => {
   return (
-    <Button $color={color} onPress={onPress} underlayColor={primary.dark}>
+    <Button disabled={disabled} $color={color} onPress={onPress} underlayColor={primary.dark}>
       <ButtonText>{title}</ButtonText>
     </Button>
   )
@@ -29,6 +30,7 @@ const Button = styled.TouchableHighlight<{ $color?: ButtonColor }>`
   align-items: center;
   justify-content: center;
   background-color: ${p => (p.$color === 'primary' ? p.theme.primary.main : p.theme.grey[800])};
+  opacity: ${p => (p.disabled ? '0.5' : 1)};
   height: 50px;
   border-radius: 16px;
 `

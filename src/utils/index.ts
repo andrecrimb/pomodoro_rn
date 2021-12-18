d817a008b47fe340e1d1eaa2581ea2965826c169
Andre Rosa <ar@gsmk.de>
2021-12-18 16:14:26 +0100

    add Convert millisecondstimeout function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,3 +8,18 @@ export const createArr = (length: number, startIndex: number = 1, space: number 
   }
   return arr
 }
+
+export function convertMillisecondsToTimeout(secondsLeft: number) {
+  let minutes, seconds, total_minutes, total_seconds
+
+  total_seconds = secondsLeft
+  total_minutes = Math.floor(total_seconds / 60)
+
+  seconds = total_seconds % 60
+  minutes = total_minutes % 60
+
+  if (minutes > 0)
+    return `${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`
+
+  return `${seconds}`
+}

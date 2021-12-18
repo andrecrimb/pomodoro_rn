841f365ec40935b8fe6bfb40af945f23e8514f98
Andre Rosa <ar@gsmk.de>
2021-12-18 18:31:31 +0100

    Fix EmptyTimerList styles

--- src/components/EmptyTimerList.tsx
+++ src/components/EmptyTimerList.tsx
@@ -9,7 +9,6 @@ import Animated, {
 } from 'react-native-reanimated'
 import { grey } from '../theme'
 import i18n from '../i18n'
-import { StyleSheet } from 'react-native'
 import { AntDesign } from '@expo/vector-icons'
 
 export default () => {
@@ -30,7 +29,7 @@ export default () => {
   return (
     <Wrapper>
       <TextMessage>{i18n.t('create_first_timer')}</TextMessage>
-      <Animated.View style={[styles.arrowWrapper, animatedArrowStyle]}>
+      <Animated.View style={animatedArrowStyle}>
         <AntDesign name="arrowdown" size={60} color={grey[500]} />
       </Animated.View>
     </Wrapper>
@@ -42,7 +41,7 @@ const Wrapper = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  min-height: 500px;
+  min-height: 600px;
 `
 const TextMessage = styled.Text`
   color: ${p => p.theme.grey[500]};
@@ -53,10 +52,4 @@ const TextMessage = styled.Text`
   margin-top: 40px;
   font-family: ${p => p.theme.font[500]};
 `
-const styles = StyleSheet.create({
-  arrowWrapper: {
-    position: 'absolute',
-    bottom: 0
-  }
-})
 //#endregion
